99236040cd7ea789556d050717e75f025cc3e851
cesslys <159104996+cesslys@users.noreply.github.com>
2024-06-02 07:43:45 +0800

    Add files via upload

---
+++ additional_header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/homepage.css">
+    <title>Document</title>
+</head>
+<body>
+<!--===============Border=================--> 
+        <div class="additional-border">
+            <div class="title-content">
+                <div class="title_text"> 
+                    <span style="font-weight: bold;">WRISTIQUE</span><br>
+                    <span>WATCHES</span>
+                </div>     
+            <img class="img" src="../webpics/WatchHomepageImage1.jpg" alt="Watch Image3" >
+                <div class="title_text2"> 
+                    THE COLLECTION
+                </div>
+            </div>
+        </div>
+        <div class="container-content">
+            <div class="content1">
+                Explore the Wristique <br> Collection</h1>
+            </div>
+            <div class="content2">
+                <div class="content2-text1">
+                        The Wristique collection offers a wide range of prestigious,<br>
+                        high-precision timepieces, from Professional to Classic<br>
+                        models to suit any wrist.
+                </div>
+                
+                </div>
+                
+            </div>
+            
+            <div class="content2-text2">
+                        Find your Wristique>
+        </div>
+
+</body>
+</html>
